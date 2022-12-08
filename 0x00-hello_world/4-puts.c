b79829b99841bac0771d5232b5a1de9af27785a4
murunga727 <sammymakey@gmail.com>
2022-12-08 10:23:05 -0800

    betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- This the program's entry point
+ * Description: C program using puts
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
