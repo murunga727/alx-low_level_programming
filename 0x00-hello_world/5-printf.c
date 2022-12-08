382b0f19ea1eeab84f5cf560d0b1d3b09b78a3a9
murunga727 <sammymakey@gmail.com>
2022-12-08 10:57:15 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- This is the program's entry point
+ * Descripion: C programusing prints
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
