b108b32767fd7b2924f76c79ee7fc29d00e6704a
murunga727 <sammymakey@gmail.com>
2022-12-13 11:28:59 -0800

    0 putcher

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  main - entry point
+ *
+ *  Description: print _putchar
+ *
+ *  Return: Always 0
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int a;
+
+	for (a = 0; a < 8; ++a)
+		_putchar(str[a]);
+	_putchar('\n');
+
+	return (0);
+}
