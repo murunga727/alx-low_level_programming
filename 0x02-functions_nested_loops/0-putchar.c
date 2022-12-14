71b00444af16d9979f2f2fc78dc32ea9de990566
murunga727 <sammymakey@gmail.com>
2022-12-14 06:06:20 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,14 @@
  */
 int main(void)
 {
-	char str[] = "_putchar";
-	int a;
-
-	for (a = 0; a < 8; ++a)
-		_putchar(str[a]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
