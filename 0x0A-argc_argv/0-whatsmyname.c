cfa314b6993842ee1c8484c6376bbe8c361caf5b
murunga727 <sammymakey@gmail.com>
2023-01-09 18:03:52 -0800

    main.h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: an int
+ * @argv: pointer to a list
+ * Return: 0(success)
+ */
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
